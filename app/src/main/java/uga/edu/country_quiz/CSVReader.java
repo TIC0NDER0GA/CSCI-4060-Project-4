219624924fdba875044fd1d259a25ef1f8c62948
TIC0NDER0GA <dey60704@uga.edu>
2023-03-27 15:26:47 -0400

    Began CSV reading class, Added the csv file for project

---
+++ app/src/main/java/uga/edu/country_quiz/CSVReader.java
@@ -0,0 +1,15 @@
+package uga.edu.country_quiz;
+
+
+/**
+ * A Utility class for reading in the contents
+ * of the country_continent csv file.
+ */
+public class CSVReader {
+
+    String fileName; // file path directory
+
+    public CSVReader() {
+
+    }
+}
