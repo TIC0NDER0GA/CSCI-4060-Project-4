ca92662aecdb8950e0ec3a03e9c3bb21341fdd85
gennyyoung <gyoung9825@gmail.com>
2023-03-30 20:44:23 -0400

    Added constructors for Quiz and Question classes

--- app/src/main/java/uga/edu/country_quiz/Question.java
+++ app/src/main/java/uga/edu/country_quiz/Question.java
@@ -1,19 +1,44 @@
 package uga.edu.country_quiz;
 
+import java.util.Random;
+
+/**
+ * A POJO to represent a Question and save the country and continents
+ */
 public class Question {
 
     private String country;
-
     private String rightC;
-
     private String wrongC1;
-
     private String wrongC2;
 
-    public Question(String country, String rightC, String wrongC1,String wrongC2) {
+    final private String[] continents = new String[] {"South America", "North America", "Asia",
+            "Africa", "Oceania", "Europe"};
+
+    /**
+     * Constructor for a question instance
+     * Randomly assigns the 2 incorrect continent choices
+     * @param country the chosen country which is passed in from the quiz instance
+     * @param rightC the correct continent, also passed in from the quiz instance
+     */
+    public Question(String country, String rightC) {
         this.country = country;
         this.rightC = rightC;
-        this.wrongC1 = wrongC1;
-        this.wrongC2 = wrongC2;
+
+        Random rand = new Random();
+        int i;
+        while (wrongC1 == null) {
+            i = rand.nextInt(6);
+            if (!continents[i].equals(rightC)) {
+                wrongC1 = continents[i];
+            }
+        }
+        while (wrongC2 == null) {
+            i = rand.nextInt(6);
+            if (!continents[i].equals(rightC) && !continents[i].equals(wrongC1)) {
+                wrongC2 = continents[i];
+            }
+        }
     }
+
 }
--- app/src/main/java/uga/edu/country_quiz/Quiz.java
+++ app/src/main/java/uga/edu/country_quiz/Quiz.java
@@ -1,15 +1,37 @@
 package uga.edu.country_quiz;
+import java.util.Random;
 
 public class Quiz {
 
-    private Question q1;
-    private Question q2;
-    private Question q3;
-    private Question q4;
-    private Question q5;
-    private Question q6;
+//    private Question q1;
+//    private Question q2;
+//    private Question q3;
+//    private Question q4;
+//    private Question q5;
+//    private Question q6;
+
+    private Question[] questions = new Question[6];
 
     private String dt; // date quiz was taken
     private double sr;  // score on quiz
     private int numQs;  // num questions answered so far
+
+    public Quiz(String[][] data, int numCountries) {
+        Random rand = new Random();
+        int randNum = 0;
+        for (int i = 0; i < 6; i++) {
+            boolean duplicateFound = true;
+            while (duplicateFound) {
+                randNum = rand.nextInt(numCountries);
+                for (int j = 0; j < data.length; j++) {
+                    if (!data[randNum][0].equals(data[j][0])) {
+                        duplicateFound = false;
+                    }
+                }
+            }
+            questions[i] = new Question(data[randNum][0], data[randNum][1]);
+
+        }
+
+    }
 }
