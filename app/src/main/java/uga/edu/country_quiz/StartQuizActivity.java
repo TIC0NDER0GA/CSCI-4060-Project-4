199c65afbc8664186b3a333aff9815c6e7138eed
gennyyoung <gyoung9825@gmail.com>
2023-04-03 12:28:26 -0400

    Added splash screen textViews/buttons in main activity (java/xml)

--- app/src/main/java/uga/edu/country_quiz/StartQuizActivity.java
+++ app/src/main/java/uga/edu/country_quiz/StartQuizActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class startQuiz extends AppCompatActivity {
+public class StartQuizActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
